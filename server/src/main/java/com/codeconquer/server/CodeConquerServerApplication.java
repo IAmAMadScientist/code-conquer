ab0c440d2dbd2706734f790ce613b3077aaeb932
IAmAMadScientist <129318011+IAmAMadScientist@users.noreply.github.com>
2025-12-04 14:51:37 +0100

    First commit

---
+++ server/src/main/java/com/codeconquer/server/CodeConquerServerApplication.java
@@ -0,0 +1,13 @@
+package com.codeconquer.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeConquerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeConquerServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codeconquer/server/CodeConquerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeconquer.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConquerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
